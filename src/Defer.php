6866c9b7f1f125f3ae4cffab2429d1b3a791fa12
hugolgs-dev <hugo.ligneres@orange.fr>
2024-12-06 23:33:03 +0100

    Defer.php (sans bonus)

--- src/Defer.php
+++ src/Defer.php
@@ -12,8 +12,51 @@ class Callback {
 }
 
 class Defer {
+    private mixed $cb_defer ;
+
     public function __construct() {}
 
-    public function __destruct() {}
+    /* Ajout d'un constructeur statique --> plus besoin après la question 7 ?
+    public static function init(): self {
+        return new self();
+    } */
+
+    /*
+     * Méthode pour passer le callable dans la méthode defer
+     * plutôt que pendant la création de l'instance
+     *
+     */
+    public function __invoke(callable $cb, ...$args): void {
+        $this->cb_defer[] = new Callback($cb, $args);
+    }
+
+    public function __destruct() {
+        // Mise en place du LIFO/DEPS
+        // On traverse le tableau (la pile?) dans le sens inverse
+        while (!empty($this->cb_defer)) {
+            $instanceDeCallback = array_pop($this->cb_defer);
+            // On fait un appel au callable
+            $instanceDeCallback->call();
+        }
+
+    }
 }
-?>
+
+// Example
+
+$defer = new Defer();
+
+echo "Pile d'exemple : " . PHP_EOL;
+
+$defer(function (int $chiffre = 1) {
+    echo $chiffre . PHP_EOL;
+});
+
+$defer(function (int $chiffre = 2) {
+    echo $chiffre . PHP_EOL;
+});
+
+$defer(function (int $chiffre = 3) {
+    echo $chiffre . PHP_EOL;
+});
+
